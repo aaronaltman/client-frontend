118797a0153bcce21695b1a13c95679bb13a1cdc
aka <aaronkaltman@gmail.com>
2024-01-24 19:23:40 -0500

    todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+
+type Todo = {
+  id: number;
+  title: string | null;
+  content: string | null;
+  created_at: string | null;
+  priority: string | null;
+};
 
 export default function Home() {
+  const [todoItems, setTodoItems] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    // Async function to fetch Todo items
+    const fetchTodos = async () => {
+      try {
+        // Ensure the URL is correct and points to your Django backend
+        const res = await fetch(
+          "http://127.0.0.1:8000/api/todo/todos/?format=json"
+        );
+        if (!res.ok) throw new Error("Network response was not ok");
+        const data = await res.json();
+        setTodoItems(data);
+      } catch (error) {
+        console.error("Failed to fetch Todo items:", error);
+        // Handle errors or set error state here
+      }
+    };
+
+    // Call the async function
+    fetchTodos();
+  }, []);
+
   return (
     <main>
       <h1>Welcome Home Friends</h1>
+      {todoItems.length > 0 ? (
+        todoItems.map((item: Todo) => (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.content}</p>
+          </div>
+        ))
+      ) : (
+        <p>No Todo items found.</p>
+      )}
     </main>
   );
 }
