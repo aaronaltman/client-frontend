c2f954ca79915b127123806018ced1bff1a288e2
aka <aaronkaltman@gmail.com>
2024-01-24 19:09:30 -0500

    update

---
+++ src/app/api/todo/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function POST() {
+  return NextResponse.redirect("/api/todo");
+}
+export function GET() {
+  return NextResponse.redirect("/api/todo");
+}
+export function PUT() {
+  return NextResponse.redirect("/api/todo");
+}
+export function DELETE() {
+  return NextResponse.redirect("/api/todo");
+}
